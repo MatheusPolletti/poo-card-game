282b5e889dba347cc65ed956fba1d30e83129fa4
Henrique Dezani <professorhenriquedezani@gmail.com>
2025-02-21 14:55:47 -0300

    Métodos de energia implementados

--- Jogador.cs
+++ Jogador.cs
@@ -1,6 +1,27 @@
 public class Jogador
 {
-    public string Nome { get; set; }
-    public int Energia {get; set;}
-    public int Vidas { get; set; }
+    public string Nome { get; }
+    public int Energia { get; private set; }
+    public int Vidas { get; private set; }
+
+    public Jogador(string nome) {
+        Nome = nome;
+        Energia = 10;
+        Vidas = 30;
+    }
+
+    // exercícios:
+    // método que consuma a energia do jogador.
+    public void ConsumirEnergia(Carta carta) {
+        Energia = Math.Max(0, this.Energia - carta.Energia);
+    }
+
+    // método que restaura a energia do jogador em 2 pontos, no máximo 10.
+    public void RestaurarEnergia() {
+        Energia = Math.Min(10, this.Energia + 2);
+    }
+
+    // exercícios:
+    // restaurar a vida de acordo com uma carta.
+    // receber um dano, de acordo com uma carta.
 }
